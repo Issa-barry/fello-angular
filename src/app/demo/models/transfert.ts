277344366905f41d911ee926a22ae8c4e30fbad4
Issa-barry <issabarry67@gmail.com>
2025-03-10 17:14:08 +0100

    optimisation

--- src/app/demo/models/transfert.ts
+++ src/app/demo/models/transfert.ts
@@ -3,10 +3,12 @@
     id?: number;
     code?:string;
     statut?:string;
+    frais?:number;
+    total?:number;
     devise_source_id: number;  // id = 2 = euro
     devise_cible_id: number;   // id  = 3 = franc guinéen
-    montant: number;
-    montant_converti?: number;
+    montant_expediteur: number;
+    montant_receveur?: number;
     quartier: string;
     receveur_nom: string;
     receveur_prenom: string;
@@ -21,8 +23,10 @@
     {
         this.devise_source_id = 2;
         this.devise_cible_id = 3;
-        this.montant=0;
-        this.montant_converti=0;
+        this.frais = 0;
+        this.total=0;
+        this.montant_expediteur=0;
+        this.montant_receveur=0;
         this.quartier="";
         this.receveur_prenom="";
         this.receveur_nom="";
