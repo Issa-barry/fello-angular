c190e4c0375e8cd4436436da1423902fc9668b0c
Issa-barry <issabarry67@gmail.com>
2025-03-10 19:55:57 +0100

    maj

--- src/app/demo/models/transfert.ts
+++ src/app/demo/models/transfert.ts
@@ -10,29 +10,25 @@
     montant_expediteur: number;
     montant_receveur?: number;
     quartier: string;
-    receveur_nom: string;
-    receveur_prenom: string;
+    receveur_nom_complet: string;
     receveur_phone: string;
-    expediteur_nom: string;
-    expediteur_prenom: string;
+    expediteur_nom_complet: string;
     expediteur_phone: string;
     expediteur_email: string;
     
 
     constructor()
     {
-        this.devise_source_id = 2;
-        this.devise_cible_id = 3;
+        this.devise_source_id = 1;
+        this.devise_cible_id = 2;
         this.frais = 0;
         this.total=0;
         this.montant_expediteur=0;
         this.montant_receveur=0;
         this.quartier="";
-        this.receveur_prenom="";
-        this.receveur_nom="";
+        this.receveur_nom_complet="";
         this.receveur_phone="";
-        this.expediteur_nom="";
-        this.expediteur_prenom="";
+        this.expediteur_nom_complet="";
         this.expediteur_phone="";
         this.expediteur_email="";
     }
